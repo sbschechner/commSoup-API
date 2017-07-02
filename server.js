fb10288c059d7483016600fbe9e1a6615fc9554f
Think <think@Thinks-MacBook-Air.local>
2017-07-02 11:27:50 -0400

    connected heroku, travis and mock server

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const express = require('express');
 
  const PORT = process.env.PORT || 3000;
 
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
-
  app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+ app.get('/api/*', (req, res) => {
+   res.json({hello: "yes it is me"});
+ });
+
+
 
  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
